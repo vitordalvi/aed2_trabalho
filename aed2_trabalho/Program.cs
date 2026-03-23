f0c7d4861e17f6b5cb1f57dd14a463737f2db14f
Vitor Dalvi <vitodalvi@gmail.com>
2026-03-23 16:13:04 -0300

    feat: added dependency injection in project

--- aed2_trabalho/Program.cs
+++ aed2_trabalho/Program.cs
@@ -5,14 +5,45 @@
     using aed2_trabalho.Services;
     using aed2_trabalho.View;
     using aed2_trabalho.Entities;
+    using aed2_trabalho.Repositories;
 
     class Program
     {
         public static void Main(string[] args)
         {
-            //ViewController.Iniciar();
+            // Banco de dados
             DatabaseInitializer.VerifyDatabase();
-            DbService.LoadData();
+            DbService dbContext = new DbService();
+            dbContext.LoadData();
+
+            // Repositorios
+            IAlunoRepository alunosRepository = new AlunoRepository(dbContext);
+            IDisciplinasRepository disciplinasRepository = new DisciplinasRepository(dbContext);
+            IMatriculasRepository matriculasRepository = new MatriculasRepository(dbContext);
+
+            // Services
+            AlunosService alunosService = new AlunosService(alunosRepository);
+            DisciplinasService disciplinasService = new DisciplinasService(disciplinasRepository);
+            MatriculasService matriculasService = new MatriculasService(matriculasRepository);
+
+            // Controllers
+            AlunosController alunosController = new AlunosController(alunosService);
+            DisciplinasController disciplinasController = new DisciplinasController(disciplinasService);
+            MatriculasController matriculasController = new MatriculasController(matriculasService);
+
+            // Views
+            AlunosView alunosView = new AlunosView(alunosController);
+            DisciplinasView disciplinasView = new DisciplinasView(disciplinasController);
+            MatriculasView matriculasView = new MatriculasView(matriculasController);
+            InicioView inicioView = new InicioView();
+            
+
+            // Controle geral Views
+            ViewService viewService = new ViewService(inicioView, alunosView, disciplinasView, matriculasView);
+            ViewController viewController = new ViewController(viewService);
+
+            // Iniciar sistema
+            viewController.Iniciar();
         }
     }
 }
