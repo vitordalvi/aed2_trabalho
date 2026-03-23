8f307315f5a40e8016a7c6d6c8a77c164aa936d4
Vitor Dalvi <vitodalvi@gmail.com>
2026-03-23 16:11:33 -0300

    fix: interfaces according to new pattern

--- aed2_trabalho/Repositories/IAlunoRepository.cs
+++ aed2_trabalho/Repositories/IAlunoRepository.cs
@@ -1,9 +1,12 @@
 ﻿using aed2_trabalho.entities;
+using aed2_trabalho.Entities;
 
 namespace aed2_trabalho.Repositories
 {
     public interface IAlunoRepository
     {
-        public void CreateAluno(string nome, int idade);
+        public bool AddAluno(string nome, int idade);
+        public bool DeleteAluno(int matricula);
+        public Alunos GetAlunoByMatricula(int matricula);
     }
 }
