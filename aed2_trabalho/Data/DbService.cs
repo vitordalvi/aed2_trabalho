a0d69ee2499c69ce114cce4aba02b1f188255cd9
Vitor Dalvi <vitodalvi@gmail.com>
2026-03-28 00:26:51 -0300

    feat: created total value of entities to increase id number fix: refact wrong switch case, now it will always access the right file using Path.GetFileNameWithoutExtension(). added auto increase id to read file

--- aed2_trabalho/Data/DbService.cs
+++ aed2_trabalho/Data/DbService.cs
@@ -10,6 +10,11 @@ namespace aed2_trabalho.Data
         public int NUMEROS_DISCIPLINAS = 0;
         public int NUMEROS_MATRICULAS = 0;
 
+        // Valor atual das entidades
+        public int TOTAL_ALUNOS = 0;
+        public int TOTAL_DISCIPLINAS = 0;
+        public int TOTAL_MATRICULAS = 0;
+
         // Criação dos vetores que conterão os objetos
         public Alunos[] alunos = new Alunos[1000];
         public Disciplinas[] disciplinas = new Disciplinas[1000];
@@ -37,11 +42,17 @@ namespace aed2_trabalho.Data
                 // Passa por cada linha
                 for (int j = 0; j < lines.Length; j++)
                 {
+                    // Se a posição da linha for nula ou vazia, ignora (validação meio parcial pq na teoria isso não pode acontecer 
+                    if (string.IsNullOrWhiteSpace(lines[j]))
+                    {
+                        throw new Exception($"O índice {j} do arquivo {dbPaths[0]} está vazio ou é nulo.");
+                    }
+
                     // Separa os dados da linha em um vetor
                     string[] data = lines[j].Split(';');
                     
                     // Faz a separação da leitura de acordo com o tipo do arquivo, para criar os objetos certinho
-                    switch (dbPaths[i].Split('.')[0])
+                    switch (Path.GetFileNameWithoutExtension(dbPaths[i]))
                     {
                         // Se for alunos, cria o objeto do aluno
                         case "Alunos":
@@ -50,6 +61,7 @@ namespace aed2_trabalho.Data
                             alunos[NUMEROS_ALUNOS] = aluno;
                             // Aumento no número do índice
                             NUMEROS_ALUNOS++;
+                            TOTAL_ALUNOS++;
                             break;
 
                             // Mesma lógica
@@ -57,6 +69,7 @@ namespace aed2_trabalho.Data
                             Disciplinas disciplina = new Disciplinas(int.Parse(data[0]), data[1], double.Parse(data[2]));
                             disciplinas[NUMEROS_DISCIPLINAS] = disciplina;
                             NUMEROS_DISCIPLINAS++;
+                            TOTAL_DISCIPLINAS++;
                             break;
 
                             // Mesma lógica
@@ -64,6 +77,7 @@ namespace aed2_trabalho.Data
                             Matriculas matricula = new Matriculas(int.Parse(data[0]), int.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3]));
                             matriculas[NUMEROS_MATRICULAS] = matricula;
                             NUMEROS_MATRICULAS++;
+                            TOTAL_MATRICULAS++;
                             break;
                     }
                 }
