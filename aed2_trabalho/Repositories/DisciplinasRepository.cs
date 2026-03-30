8cc9aa898cf28cd732d817d0299c4931be1de36e
Vitor Dalvi <vitodalvi@gmail.com>
2026-03-30 11:12:03 -0300

    feat: added GetDisciplinaById method

--- aed2_trabalho/Repositories/DisciplinasRepository.cs
+++ aed2_trabalho/Repositories/DisciplinasRepository.cs
@@ -1,4 +1,5 @@
 ﻿using aed2_trabalho.Data;
+using aed2_trabalho.entities;
 
 namespace aed2_trabalho.Repositories
 {
@@ -11,6 +12,31 @@ namespace aed2_trabalho.Repositories
             _dbContext = dbContext;
         }
 
+        public Disciplinas GetDisciplinaById(int codigoDisciplina)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(_dbContext.dbPaths[2]);
 
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] data = lines[i].Split(';');
+
+                    if (data[0] == codigoDisciplina.ToString())
+                    {
+                        Disciplinas disciplina = new Disciplinas(int.Parse(data[0]), data[1], int.Parse(data[2]));
+
+                        return disciplina;
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            throw new Exception("Disciplina não encontrada pelo código.");
+        }
     }
 }
